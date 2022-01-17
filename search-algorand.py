104a6247b1598f292f3ca03e686a2eddbb842351
N3koSempai <alvarom12098@gmail.com>
2022-01-17 14:33:31 -0500

    basic script only one thread and use .txt for save the result

---
+++ search-algorand.py
@@ -0,0 +1,48 @@
+from algosdk import account
+import threading
+import requests
+import json
+
+
+
+class Algobot():
+
+    def __init__(self):
+        self.address = ''
+        self.private_key = ''
+        self.url_explorer = 'https://algoindexer.algoexplorerapi.io/v2/accounts/'
+
+    def generate_algorand_keypair(self):
+        self.private_key, self.address = account.generate_account()
+
+    def check(self, iter):
+        #request info from the api
+        response = (requests.get(self.url_explorer + self.address))
+        # take only the atribut amount
+        if response.status_code != 404:
+            rs = json.loads(response.content)
+            amount = rs['account']['amount']
+            #check if the address have money and save in a file called match.txt
+            file = open('match.txt', 'a')
+            if amount > 0:
+                file.write('address:' + self.address + ' private_key ' + self.private_key)
+                return 1
+        else:
+            print('nothing in the ', iter, ' iteration')
+            return 0
+
+algo = Algobot()
+try:
+    Max = int(input("write the maximun iteration(10 by default): ") or '10')
+except:
+    print('you have an eror, only write digits')
+match = 0
+for i in range(0, Max):
+    algo.generate_algorand_keypair()
+    result = algo.check(i)
+    match = match + result
+print("\n ITERATION FINISH: \n MATCH: ", match)
+
+
+
+
